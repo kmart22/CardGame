6ec6a215edac990e88354fe0dba9e72a572d740f
kmart22 <kmar7070@740s-pl-111.local>
2016-10-31 08:22:15 -0600

    added deck and playingcarddeck to model

---
+++ CardGame/PlayingCardDeck.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCardDeck.swift
+//  CardGame
+//
+//  Created by Martinsen, Kaden on 10/31/16.
+//  Copyright © 2016 Martinsen, Kaden. All rights reserved.
+//
+
+import Foundation
---
+++ Deck.swift
@@ -0,0 +1,9 @@
+//
+//  Deck.swift
+//  CardGame
+//
+//  Created by Martinsen, Kaden on 10/31/16.
+//  Copyright © 2016 Martinsen, Kaden. All rights reserved.
+//
+
+import Foundation
